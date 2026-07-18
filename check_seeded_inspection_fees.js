eb391885da45e46997b53bc03c45962aad9b7916
beingessan01-del <beingessan01-del@users.noreply.github.com>
2026-07-18 19:12:19 +0500

    Cleanup script

--- check_seeded_inspection_fees.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('lib/data.ts', 'utf8');
-const lines = content.split('\n');
-
-for (let i = 0; i < lines.length; i++) {
-  if (lines[i].includes('inspectionFee')) {
-    console.log(`Line ${i+1}: ${lines[i].trim()}`);
-  }
-}
